78f4dfe6c6e80f5a2c551c95f7a640328616fb57
Delza Ananjaya <99465619+Delzaananjaya@users.noreply.github.com>
2022-05-21 12:17:33 +0700

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -22,7 +22,7 @@ export default function HomePage({ products }) {
         <title>TOKO BIRU | Situs Jual Beli Online Terlengkap, Murah & Aman</title>
         <meta
           name="description"
-          content="Shoply merupakan web e-commerce yang dibuat oleh Muhammad Raihan S dan Muhammad Rian. Kami berusaha mendesign dengan tema modern dan menggunakan framework Next agar pengguna dapat merasakan pengalaman yang baik."
+          content="Toko Biru merupakan web e-commerce yang dibuat oleh Delza Ananjaya. Kami berusaha mendesign dengan tema modern dan menggunakan framework Next agar pengguna dapat merasakan pengalaman yang baik."
         />
       </Head>
       {isAdmin ? (
